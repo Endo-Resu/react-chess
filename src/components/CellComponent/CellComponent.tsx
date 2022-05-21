21758975711f29a19d6dd75e24f335f2a8ad3e73
schiZis <stahanovgod@gmail.com>
2022-05-21 12:37:46 +0500

    added availability to move figures

--- src/components/CellComponent/CellComponent.tsx
+++ src/components/CellComponent/CellComponent.tsx
@@ -3,11 +3,18 @@ import {Cell} from "../../models/Cell/Cell";
 
 interface CellProps {
     cell: Cell;
+    selected: boolean;
+    click: (cell : Cell) => void;
 }
 
-export const CellComponent: FC<CellProps> = ({cell}) => {
+export const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
-        <div className={['cell', cell.colour].join(' ')}>
+        <div
+            className={['cell', cell.colour, selected ? "selected" : ''].join(' ')}
+            onClick={() => click(cell)}
+            style={{background: cell.available && cell.figure ? 'green' : ''}}
+        >
+            {cell.available && !cell.figure && <div className={"available"}/>}
             {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
         </div>
     )
